606902811de8e0131ec687a7bb08188f7cae79da
AndrGole <a9537707806@gmail.com>
2025-03-10 18:13:30 +0700

    some minor edits

--- Andrew.cpp
+++ Andrew.cpp
@@ -5,20 +5,17 @@
 bool palindrome(std::string text){
 	bool res = true;
 	std::string res_string = "";
-
 	for (int i = 0; i < text.size(); i++){
 		if(text[i] != ' '){
 			res_string = res_string + char(std::tolower(int(text[i])));
 		}
 	}
-
 	for (int i = 0; i < res_string.size() / 2; i++) {
 		if (res_string[i] != res_string[res_string.size() - i - 1]) {
 			res = false;
 			break;
 		}
 	}
-
 	return res;
 }
 
@@ -26,6 +23,5 @@ int main()
 {
 	std::string text;
 	std::getline(std::cin, text);
-
 	std::cout << "This string is " << (palindrome(text) ? "" : "not ") << "a palindrome." << std::endl;
 }
